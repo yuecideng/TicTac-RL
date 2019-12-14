01ffb210224a3a7c25eb43508cd113b033f25735
yuecideng <yuecideng@gmail.com>
2019-12-14 21:37:15 +0800

    latest update

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # TicTac-RL
 A RL based algorithm (Q-Learning&amp;Sarsa) for training an intelligent agent to play the tictac game uisng Python and Numpy
+
 # Introduction
 The reinforcement learning system is built as three parts:
-- agent - the decision maker (`QLearn.py` and `Sarsa.py`)
+- agent - the decision maker (`Qlearn` and `Sarsa`)
 - environment - the rule of tictac game (`tictac_env.py`)
 - interactive interface - where training and testing are performed (`run_tictac_RL.py`) 
+
 # Prerequisites
 ```
 python 3.6
 Numpy
 ```
+
 # Usage
-`python3 run_tictac_RL.py --algorithm Q-learning # or Sarsa`
+- Train the RL model with specific steps 
+`python3 run_tictac_RL.py --algorithm Qlearn --step 50000 --train True`
+
+- Use pre-trained model 
+`python3 run_tictac_RL.py --algorithm Qlearn  --train True`
+
 # Reference
 **Q-learning** and **Sarsa** are implemented as described in [Reinforcement Learning:
 An Introduction](https://web.stanford.edu/class/psych209/Readings/SuttonBartoIPRLBook2ndEd.pdf) (Chapter6, P154-P157)
--- run_tictac_RL.py
+++ run_tictac_RL.py
@@ -1,38 +1,54 @@
-from tictac_env import TicTac
-from QLearn import QLearn
-from Sarsa import Sarsa
 import argparse
+import pickle
 import copy
 import os
 
+from tictac_env import TicTac
+from agent import *
 
-Training_round = 100000 
+
+def save_table(model, model_name):
+    if not os.path.exists(os.path.join('model', model_name)):
+        os.makedirs(os.path.join('model', model_name))
+
+    with open(os.path.join('model', model_name, 'model.pkl'), 'wb') as f:
+        pickle.dump(model, f, pickle.HIGHEST_PROTOCOL)
+    
+
+def load_table(model_name):  
+    with open(os.path.join('model', model_name, 'model.pkl'), 'rb') as f:
+        model = pickle.load(f)
+        print('load model successfully!')
+
+    return model
+
 
 def train(args):
     env = TicTac()
-    if args.algorithm == 'Q-learning': 
-        model1 = QLearn(actions=list(range(env.n_actions))) 
-        model2 = QLearn(actions=list(range(env.n_actions))) 
+    if args.algorithm == 'Qlearn': 
+        model1 = Qlearn(actions=list(range(env.n_actions))) 
+        model2 = Qlearn(actions=list(range(env.n_actions))) 
+
     if args.algorithm == 'Sarsa': 
         model1 = Sarsa(actions=list(range(env.n_actions)))
         model2 = Sarsa(actions=list(range(env.n_actions)))
 
     first_win, second_win, tie = 0, 0, 0
-    for epoch in range(Training_round):
+    for epoch in range(args.step):
         state1 = env.reset()
         while True:
             # The first agent step and learn
             action1 = model1.choose_action(state1)
-            state1_, reward1, done = env.step(action1,state1,agent='first')
-            model1.learn(state1,action1,reward1,state1_)
+            state1_, reward1, done = env.step(action1, state1, agent='first')
+            model1.learn(state1, action1, reward1, state1_)
             while state1_ == state1:
                 action1 = model1.choose_action(state1)
-                state1_, reward1, done = env.step(action1,state1,agent='first')
-                model1.learn(state1,action1,reward1,state1_)     
+                state1_, reward1, done = env.step(action1, state1, agent='first')
+                model1.learn(state1, action1, reward1, state1_)     
             
             if done:
                 reward2, state2_ = env.lose()
-                model2.learn(state2,action2,reward2,state2_)
+                model2.learn(state2, action2, reward2, state2_)
                 first_win += 1
                 break
 
@@ -43,120 +59,138 @@ def train(args):
             # The second agent step and learn
             state2 = copy.deepcopy(state1_)
             action2 = model2.choose_action(state2)
-            state2_, reward2, done = env.step(action2,state2,agent='second')
-            model2.learn(state2,action2,reward2,state2_)
+            state2_, reward2, done = env.step(action2, state2, agent='second')
+            model2.learn(state2, action2, reward2, state2_)
             while state2_ == state2:
                 action2 = model2.choose_action(state2)
-                state2_, reward2, done = env.step(action2,state2,agent='second')
-                model2.learn(state2,action2,reward2,state2_) 
+                state2_, reward2, done = env.step(action2, state2, agent='second')
+                model2.learn(state2, action2, reward2, state2_) 
 
             if done: 
                 reward1, state1_ = env.lose()        
-                model1.learn(state1,action1,reward1,state1_)
+                model1.learn(state1, action1, reward1, state1_)
                 second_win += 1
                 break
+
             state1 = copy.deepcopy(state2_)
 
         print('first_win', first_win)
         print('second_win', second_win)
         print('tie', tie)
-        percent = float(epoch*100)/Training_round
+        percent = float(epoch*100) / args.step
         print('training process: %.2f' % percent, '%')
 
     # save Q table 
-    model1.save_table('Q_table', 'Q_learn1')
-    model2.save_table('Q_table', 'Q_learn2')
+    save_table(model1.q_table, os.path.join(args.algorithm, '1'))
+    save_table(model2.q_table, os.path.join(args.algorithm, '2'))
+
 
 def play(args):
     print('start to play the game')
     print('################')
-    print(' ' + '6' + ' | ' + '7' + ' | ' + '8')
+    print(' ' + '7' + ' | ' + '8' + ' | ' + '9')
     print('------------')
-    print(' ' + '3' + ' | ' + '4' + ' | ' + '5')
+    print(' ' + '4' + ' | ' + '5' + ' | ' + '6')
     print('------------')
-    print(' ' + '0' + ' | ' + '1' + ' | ' + '2')
+    print(' ' + '1' + ' | ' + '2' + ' | ' + '3')
     print('################')
-    print('please type 0 ~ 8 to selcet the place')
+    print('please type 1 ~ 9 to selcet the place')
     print('please select the order 1. first (you go first) or 2. second (AI go first)')
-    mode = input('mode: ')
+    mode = input('mode: ') 
     assert mode == 'first' or mode == 'second', 'please type first or second'
     
     env_play = TicTac()
-    if args.algorithm == 'Q-learning': model = QLearn(actions=list(range(env_play.n_actions))) 
+    if args.algorithm == 'Qlearn': model = Qlearn(actions=list(range(env_play.n_actions))) 
     if args.algorithm == 'Sarsa': model = Sarsa(actions=list(range(env_play.n_actions)))
+
     if mode == 'first':
-        model.load_table('Q_table', 'Q_learn2')
+        model.q_table = load_table(os.path.join(args.algorithm, '2'))
         board = env_play.reset()
         env_play.drawBoard(board)
+
         while True:
-            choice = human_input(board)
-            board[choice] = 1
-            if env_play.check_win(board) == 1:
-                env_play.drawBoard(board)
-                print('human win!')
-                break
+            try:
+                choice = human_input(board)
+                board[choice] = 1
+                if env_play.check_win(board) == 1:
+                    env_play.drawBoard(board)
+                    print('human win!')
+                    break
 
-            if env_play.check_over(board):
-                env_play.drawBoard(board)
-                print('tie! game over!')
-                break  
+                if env_play.check_over(board):
+                    env_play.drawBoard(board)
+                    print('tie! game over!')
+                    break  
 
-            action = model.choose_action(board, explore=False)
-            null = [i for i in range(len(board)) if board[i] == 0]
-            if action not in null:
-                print('human win!')
-                break
-            board[action] = 2
-            env_play.drawBoard(board)
-            if env_play.check_win(board) == 2:
+                action = model.choose_action(board, explore=False)
+                null = [i for i in range(len(board)) if board[i] == 0]
+                if action not in null:
+                    print('AI get fool!')
+                    print('human win!')
+                    break
+
+                board[action] = 2
                 env_play.drawBoard(board)
-                print('AI win!')
-                break
-            
-            if env_play.check_over(board):
-                env_play.drawBoard(board)
-                print('tie! game over!')
-                break
+                if env_play.check_win(board) == 2:
+                    env_play.drawBoard(board)
+                    print('AI win!')
+                    break
+                
+                if env_play.check_over(board):
+                    env_play.drawBoard(board)
+                    print('tie! game over!')
+                    break
+
+            except KeyboardInterrupt:
+                print('Quit game')
 
     if mode == 'second':
-        model.load_table('Q_table', 'Q_learn1')
+        model.q_table = load_table(os.path.join(args.algorithm, '1'))
         board = env_play.reset()
         env_play.drawBoard(board)
+
         while True:
-            action = model.choose_action(board, explore=False)
-            null = [i for i in range(len(board)) if board[i] == 0]
-            if action not in null:
-                print('human win!')
-                break
-            board[action] = 2
-            env_play.drawBoard(board)
-            if env_play.check_win(board) == 2:
+            try:
+                action = model.choose_action(board, explore=False)
+                null = [i for i in range(len(board)) if board[i] == 0]
+                if action not in null:
+                    print('AI get fool!')
+                    print('human win!')
+                    break
+                board[action] = 2
                 env_play.drawBoard(board)
-                print('AI win!')
-                break
+                if env_play.check_win(board) == 2:
+                    env_play.drawBoard(board)
+                    print('AI win!')
+                    break
 
-            if env_play.check_over(board):
-                env_play.drawBoard(board)
-                print('tie! game over!')
-                break  
+                if env_play.check_over(board):
+                    env_play.drawBoard(board)
+                    print('tie! game over!')
+                    break  
 
-            choice = human_input(board)
-            board[choice] = 1
-            if env_play.check_win(board) == 1:
-                env_play.drawBoard(board)
-                print('human win!')
-                break
-            
-            if env_play.check_over(board):
-                env_play.drawBoard(board)
-                print('tie! game over!')
-                break  
+                choice = human_input(board)
+                board[choice] = 1
+                if env_play.check_win(board) == 1:
+                    env_play.drawBoard(board)
+                    print('human win!')
+                    break
+                
+                if env_play.check_over(board):
+                    env_play.drawBoard(board)
+                    print('tie! game over!')
+                    break 
+
+            except KeyboardInterrupt:
+                print('Quit game') 
+
 
 def human_input(board):
     while True:
-        choice = int(input('please choice: '))
-        assert 0 <= choice <=8 , 'please type 0 to 8'
+        choice = int(input('please choice: ')) - 1
+        assert 0 <= choice <=8 , 'please type 1 to 9'
         null = [i for i in range(len(board)) if board[i] == 0]
+
         if choice not in null:
             print('please choice again!')
         else:
@@ -167,11 +201,16 @@ def human_input(board):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--algorithm', default='Q-learning')
+    parser.add_argument('--algorithm', default='Qlearn', type=str)
+    parser.add_argument('--step', default=50000, type=int, help='Total training steps')
+    parser.add_argument('--train', default=True)
     args = parser.parse_args()
-    assert args.algorithm == 'Q-learning' or args.algorithm == 'Sarsa', 'Please type Q-learning or Sarsa'
+    assert args.algorithm == 'Qlearn' or args.algorithm == 'Sarsa', 'Please type Qlearn or Sarsa'
 
-    times = 5 # number of games you want to play
-    train(args)
-    for i in range(times):
+    if args.train == True:
+        train(args)
+
+    while True:
         play(args) 
+
+    
